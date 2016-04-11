6e2a160e603389a7a32d30c3b6d880830d64c135
rezakhoie <reza90@gmail.com>
2016-04-11 01:00:22 -0600

    Create Plot1.R

---
+++ Plot1.R
@@ -0,0 +1,11 @@
+filedir<-"C://Users//reza//project//household_power_consumption.txt"
+dataa <- read.table(filedir,  sep=";",header=TRUE,dec=".", stringsAsFactors=FALSE)
+dataa$Date <- as.Date(dataa$Date, format="%d/%m/%Y")
+data <- subset(dataa, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+rm(dataa)
+datetime <- paste(as.Date(data$Date), data$Time)
+data$Datetime <- as.POSIXct(datetime)
+globalActivePower <- as.numeric(data$Global_active_power)
+hist(globalActivePower, main="Global Active Power", xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
+dev.copy(png, file="plot1.png", height=480, width=480)
+dev.off()
